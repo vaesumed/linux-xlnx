820f39a73f6b6feb892bdcc6eec50cb31663a512
Gregory Haskins <ghaskins@novell.com>
2008-02-12 13:30:05 -0500

    sched: fix cpus_allowed settings
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>
    Acked-by: Steven Rostedt <srostedt@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -182,6 +182,7 @@ void kthread_bind(struct task_struct *k, unsigned int cpu)
 	wait_task_inactive(k);
 	set_task_cpu(k, cpu);
 	k->cpus_allowed = cpumask_of_cpu(cpu);
+	k->rt.nr_cpus_allowed = 1;
 }
 EXPORT_SYMBOL(kthread_bind);
 
