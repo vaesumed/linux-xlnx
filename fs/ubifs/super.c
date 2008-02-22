7b378ff73f33cd8eb8c22d6239c1f41ba88bff2a
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-02-22 17:45:04 +0200

    UBIFS: introduce xattr-related fields to inode
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/super.c
+++ fs/ubifs/super.c
@@ -81,6 +81,15 @@ struct inode *ubifs_iget(struct super_block *sb, unsigned long inum)
 	ui->flags = le32_to_cpu(ino->flags);
 	ui->compr_type = le16_to_cpu(ino->compr_type);
 	ui->creat_sqnum = le64_to_cpu(ino->creat_sqnum);
+	ui->xattr_cnt = le32_to_cpu(ino->xattr_cnt);
+	ui->xattr_bytes = le64_to_cpu(ino->xattr_bytes);
+
+	/* Validate inode to prevent potential voulnerabilities */
+	if (inode->i_size > c->max_inode_sz) {
+		ubifs_err("inode is too large (%lld)",
+			  (long long)inode->i_size);
+		goto out_invalid;
+	}
 
 	if (inode->i_size > c->max_inode_sz) {
 		ubifs_err("inode is too large (%lld)",
@@ -89,6 +98,17 @@ struct inode *ubifs_iget(struct super_block *sb, unsigned long inum)
 	}
 	if (ui->compr_type < 0 || ui->compr_type >= UBIFS_COMPR_TYPES_CNT) {
 		ubifs_err("unknown compression type %d", ui->compr_type);
+		goto out_invalid;
+	}
+
+	if (ui->xattr_cnt < 0 || ui->xattr_bytes < 0) {
+		dbg_err("bad xattr_cnt %d or xattr_bytes %lld",
+			ui->xattr_cnt, ui->xattr_bytes);
+		goto out_invalid;
+	}
+
+	if (ui->data_len < 0 || ui->data_len > UBIFS_MAX_INO_DATA) {
+		ubifs_err("invalid inode data length %d", ui->data_len);
 		goto out_invalid;
 	}
 
