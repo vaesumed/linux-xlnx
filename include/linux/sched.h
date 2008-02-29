40066c0faff66f1341c76c08b88fa4b00800d7b4
Arjan van de Ven <arjan@linux.intel.com>
2008-02-29 06:19:15 -0800

    x86: mark init_mm deprecated
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1613,7 +1613,7 @@ static inline int kstack_end(void *addr)
 extern union thread_union init_thread_union;
 extern struct task_struct init_task;
 
-extern struct   mm_struct init_mm;
+extern struct   mm_struct  __deprecated_for_modules init_mm;
 
 extern struct pid_namespace init_pid_ns;
 
