f9a53ccc1c4e7672cd47b2cd4a7d38a5c43dea11
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-03-04 20:42:40 +0200

    UBIFS: add helper function for inode rmoval
    Just a small clean-up.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/ubifs.h
+++ fs/ubifs/ubifs.h
@@ -1348,6 +1348,7 @@ int ubifs_tnc_remove_nm(struct ubifs_info *c, const union ubifs_key *key,
 			const struct qstr *nm);
 int ubifs_tnc_remove_range(struct ubifs_info *c, union ubifs_key *from_key,
 			   union ubifs_key *to_key);
+int ubifs_tnc_remove_ino(struct ubifs_info *c, ino_t inum);
 struct ubifs_dent_node *ubifs_tnc_next_dent(struct ubifs_info *c,
 					    union ubifs_key *key,
 					    const char *name,
