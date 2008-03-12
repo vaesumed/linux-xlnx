8219aaf0747622a8e2ab816a76de215cdede53d1
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-03-12 18:32:57 +0100

    [S390] sched: Add arch_update_cpu_topology hook.
    Will be called each time the scheduling domains are rebuild.
    Needed for architectures that don't have a static cpu topology.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -6804,6 +6804,10 @@ static int ndoms_cur;		/* number of sched domains in 'doms_cur' */
  */
 static cpumask_t fallback_doms;
 
+void __attribute__((weak)) arch_update_cpu_topology(void)
+{
+}
+
 /*
  * Set up scheduler domains and groups. Callers must hold the hotplug lock.
  * For now this just excludes isolated cpus, but could be used to
@@ -6813,6 +6817,7 @@ static int arch_init_sched_domains(const cpumask_t *cpu_map)
 {
 	int err;
 
+	arch_update_cpu_topology();
 	ndoms_cur = 1;
 	doms_cur = kmalloc(sizeof(cpumask_t), GFP_KERNEL);
 	if (!doms_cur)
