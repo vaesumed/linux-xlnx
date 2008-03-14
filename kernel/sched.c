86dbe9753e45f829b3285d969e1669f019e374b4
Ingo Molnar <mingo@elte.hu>
2008-03-14 16:09:59 +0100

    sched: remove sysctl_sched_batch_wakeup_granularity
    it's unused.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -5364,7 +5364,6 @@ static inline void sched_init_granularity(void)
 		sysctl_sched_latency = limit;
 
 	sysctl_sched_wakeup_granularity *= factor;
-	sysctl_sched_batch_wakeup_granularity *= factor;
 }
 
 #ifdef CONFIG_SMP
