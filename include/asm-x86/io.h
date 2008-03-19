27dc04f8516d4aa6fbc00d66d9ff97820ffa97f6
venkatesh.pallipadi@intel.com <venkatesh.pallipadi@intel.com>
2008-03-18 17:00:15 -0700

    x86: PAT avoid aliasing in /dev/mem read/write
    Add xlate and unxlate around /dev/mem read/write. This sets up the mapping
    that can be used for /dev/mem read and write without aliasing worries.
    
    Signed-off-by: Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/io.h
+++ include/asm-x86/io.h
@@ -1,5 +1,13 @@
+#ifndef _ASM_X86_IO_H
+#define _ASM_X86_IO_H
+
 #ifdef CONFIG_X86_32
 # include "io_32.h"
 #else
 # include "io_64.h"
 #endif
+
+extern void *xlate_dev_mem_ptr(unsigned long phys);
+extern void unxlate_dev_mem_ptr(unsigned long phys, void *addr);
+
+#endif /* _ASM_X86_IO_H */
