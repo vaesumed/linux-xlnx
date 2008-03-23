4a6a240e94d29494e22d813dfca79963eef0997d
Joe Perches <joe@perches.com>
2008-03-23 01:03:44 -0700

    include/asm-x86/thread_info.h: checkpatch cleanups - formatting only
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/thread_info.h
+++ include/asm-x86/thread_info.h
@@ -8,6 +8,7 @@
 #ifndef __ASSEMBLY__
 extern void arch_task_cache_init(void);
 extern void free_thread_info(struct thread_info *ti);
-extern int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src);
+extern int arch_dup_task_struct(struct task_struct *dst,
+				struct task_struct *src);
 #endif
 #endif /* _ASM_X86_THREAD_INFO_H */
