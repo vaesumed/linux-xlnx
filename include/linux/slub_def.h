0304c4a53db464e50fb8203f22e1ee871f814b62
Vegard Nossum <vegard.nossum@gmail.com>
2008-04-04 19:26:29 +0200

    x86: kmemcheck v7
    Signed-off-by: Vegard Nossum <vegardno@ifi.uio.no>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/slub_def.h
+++ include/linux/slub_def.h
@@ -240,24 +240,12 @@ void kmemcheck_slab_alloc(struct kmem_cache *s, gfp_t gfpflags, void *object);
 void kmemcheck_slab_free(struct kmem_cache *s, void *object);
 #else
 static inline struct page *kmemcheck_allocate_slab(struct kmem_cache *s,
-	gfp_t flags, int node, int pages)
-{
-	return NULL;
-}
-
+	gfp_t flags, int node, int pages) { return NULL; }
 static inline void kmemcheck_free_slab(struct kmem_cache *s,
-	struct page *page, int pages)
-{
-}
-
+	struct page *page, int pages) { }
 static inline void kmemcheck_slab_alloc(struct kmem_cache *s,
-	gfp_t gfpflags, void *object)
-{
-}
-
-static inline void kmemcheck_slab_free(struct kmem_cache *s, void *object)
-{
-}
-#endif
+	gfp_t gfpflags, void *object) { }
+static inline void kmemcheck_slab_free(struct kmem_cache *s, void *object) { }
+#endif /* CONFIG_KMEMCHECK */
 
 #endif /* _LINUX_SLUB_DEF_H */
