f19bf407fe7351a4758a40fbe2e79815b02d514a
David Vrabel <david.vrabel@csr.com>
2008-04-08 13:24:46 -0700

    bitmap: add bitmap_copy_le()
    bitmap_copy_le() copies a bitmap, putting the bits into little-endian
    order (i.e., each unsigned long word in the bitmap is put into
    little-endian order).
    
    The UWB stack used bitmaps to manage Medium Access Slot availability,
    and these bitmaps need to be written to the hardware in LE order.
    
    Signed-off-by: David Vrabel <david.vrabel@csr.com>
    Cc: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/bitmap.h
+++ include/linux/bitmap.h
@@ -123,6 +123,7 @@ extern int bitmap_bitremap(int oldbit,
 extern int bitmap_find_free_region(unsigned long *bitmap, int bits, int order);
 extern void bitmap_release_region(unsigned long *bitmap, int pos, int order);
 extern int bitmap_allocate_region(unsigned long *bitmap, int pos, int order);
+extern void bitmap_copy_le(unsigned long *dst, const unsigned long *src, int nbits);
 
 #define BITMAP_LAST_WORD_MASK(nbits)					\
 (									\
