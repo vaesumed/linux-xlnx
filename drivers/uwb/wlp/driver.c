299450b8e9688c23a5bc523b04d02cf1b05311a0
Greg Kroah-Hartman <gregkh@suse.de>
2008-04-08 13:24:46 -0700

    wusb: add the wireless usb stack to Linux
    TODO:
    	- minor cleanups
    	- sysfs usage isn't "optimal"
    	- remove build #warnings
    	- clean up the #ifdefs for kernel versions
    
    Cc: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

---
+++ drivers/uwb/wlp/driver.c
@@ -0,0 +1,52 @@
+/*
+ * WiMedia Logical Link Control Protocol (WLP)
+ *
+ * Copyright (C) 2007 Intel Corporation
+ * Reinette Chatre <reinette.chatre@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ *
+ * Life cycle of WLP substack
+ *
+ * FIXME: Docs
+ */
+
+#include <linux/device.h>
+#include <linux/uwb.h>
+#include "../uwb-version.h"
+
+
+static
+int __init wlp_subsys_init(void)
+{
+	int result = 0;
+
+	return result;
+}
+module_init(wlp_subsys_init);
+
+static
+void __exit wlp_subsys_exit(void)
+{
+	return;
+}
+module_exit(wlp_subsys_exit);
+
+MODULE_AUTHOR("Reinette Chatre <reinette.chatre@intel.com>");
+MODULE_DESCRIPTION("WiMedia Logical Link Control Protocol (WLP)");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(LUWB_VERSION);
+
---
+++ include/linux/uwb/compat.h
@@ -0,0 +1,116 @@
+/*
+ * UWB Stack
+ * Compatibility with older Linux kernels
+ *
+ * This file is provided under a dual BSD/GPLv2 license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * NOTE: These copyright notices apply only to code which was
+ *       originally created, not the ones that were copied from the
+ *       Linux kernel to provide compatibility. Those retain their
+ *       originating license.
+ *
+ * GPL LICENSE SUMMARY
+ *
+ * Copyright(c) 2007 Intel Corporation. All rights reserved.
+ * Copyright(c) 2007 Samyoung Electronics. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * The full GNU General Public License is included in this distribution
+ * in the file called LICENSE.GPL.
+ *
+ *
+ * BSD LICENSE
+ *
+ * Copyright(c) 2007 Samyoung Electronics. All rights reserved.
+ * Copyright(c) 2007 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation, nor Samyoung
+ *     Electronics, nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Compatibility layer for Linux UWB vs. other Linux kernel
+ * versions. We try to keep this down to a minimum. Much of this is
+ * stolen from the newer kernels we try to emulate.
+ *
+ *
+ * (C) 2007 JaeHee Kim <max27@samyoung.co.kr>
+ *
+ *     First implementation
+ *
+ * (C) 2007 Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
+ *
+ *     Cleanups and breakups in compat blocks per each kernel version
+ *     (approximated) and other dirty hacks and additions to make it
+ *     happen.
+ *
+ * (C) Others
+ *
+ *     Snippets from the Linux kernel.
+ *
+ *
+ * @lket@ignore-file -- We actually don't want this file for upstream :)
+ */
+#ifndef __LINUX_UWB_COMPAT_H__
+#define __LINUX_UWB_COMPAT_H__
+#include <linux/version.h>
+#include <linux/uwb/config.h>
+
+/* Some things beat me ... */
+#ifdef CONFIG_ARM
+#define __SEMAPHORE_INITIALIZER(name, cnt) __SEMAPHORE_INIT(name, cnt)
+#endif
+
+#define netdev_get_kobj(net_dev) &(net_dev)->dev.kobj
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,22)
+// From 2.6.21 to 2.6.22, bus_type->subsys is a kset, ugly tweak
+#define subsys subsys.kset
+// in 2.6.22, skb->mac dissapears and we use accessors
+#define skb_mac_header(skb) ((void*)skb->mac.raw)
+#endif /* #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,22) */
+
+
+#ifdef NEED_BITMAP_COPY_LE
+extern void bitmap_copy_le(unsigned long *dst, const unsigned long *src, int nbits);
+#endif
+
+
+#endif /* end of __LINUX_UWB_COMPAT_H__ */
+
+
