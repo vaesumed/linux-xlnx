f81c6447fa56df0b127586eeb0d0afcd5103ac8b
Stephen Rothwell <sfr@canb.auug.org.au>
2008-04-09 14:38:43 +1000

    acpi-fix-2
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/x86/kernel/setup_64.c
+++ arch/x86/kernel/setup_64.c
@@ -1136,18 +1136,3 @@ static __init int setup_disablecpuid(char *arg)
 	return 1;
 }
 __setup("clearcpuid=", setup_disablecpuid);
-
-#ifdef CONFIG_X86_TRAMPOLINE
-/*
- * Currently trivial. Write the real->protected mode
- * bootstrap into the page concerned. The caller
- * has made sure it's suitably aligned.
- */
-
-unsigned long setup_trampoline(void)
-{
-	void *tramp = __va(TRAMPOLINE_BASE);
-	memcpy(tramp, trampoline_data, trampoline_end - trampoline_data);
-	return virt_to_phys(tramp);
-}
-#endif /* CONFIG_X86_TRAMPOLINE */
