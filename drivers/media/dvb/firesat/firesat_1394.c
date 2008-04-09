fe0cb49404e49297913a9a3ff3bbdc76ebd59dca
Manu Abraham <manu@linuxtv.org>
2008-04-09 07:41:21 +0400

    7365
    Beautify info struct
    
    Cc: Christian Dolzer <c.dolzer@digital-everywhere.com>
    Cc: Andreas Monitzer <andy@monitzer.com>
    Cc: Fabio De Lorenzo <delorenzo.fabio@gmail.com>
    Cc: Robert Berger <robert.berger@reliableembeddedsystems.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/media/dvb/firesat/firesat_1394.c
+++ drivers/media/dvb/firesat/firesat_1394.c
@@ -149,40 +149,59 @@ static struct dvb_frontend_ops firesat_ops = {
 /* End of dvb_frontend_ops */
 
 static struct dvb_frontend_info firesat_S_frontend_info = {
+
 	.name			= "FireSAT DVB-S Frontend",
 	.type			= FE_QPSK,
+
 	.frequency_min		= 950000,
 	.frequency_max		= 2150000,
 	.frequency_stepsize	= 125,
 	.symbol_rate_min	= 1000000,
 	.symbol_rate_max	= 40000000,
-	.caps = FE_CAN_INVERSION_AUTO | FE_CAN_FEC_1_2 | FE_CAN_FEC_2_3 |
-		FE_CAN_FEC_3_4 | FE_CAN_FEC_5_6 | FE_CAN_FEC_7_8 |
-		FE_CAN_FEC_AUTO | FE_CAN_QPSK,
+
+	.caps 			= FE_CAN_INVERSION_AUTO		|
+				  FE_CAN_FEC_1_2		|
+				  FE_CAN_FEC_2_3		|
+				  FE_CAN_FEC_3_4		|
+				  FE_CAN_FEC_5_6		|
+				  FE_CAN_FEC_7_8		|
+				  FE_CAN_FEC_AUTO		|
+				  FE_CAN_QPSK,
 };
 
 static struct dvb_frontend_info firesat_C_frontend_info = {
+
 	.name			= "FireSAT DVB-C Frontend",
 	.type			= FE_QAM,
+
 	.frequency_min		= 47000000,
 	.frequency_max		= 866000000,
 	.frequency_stepsize	= 62500,
 	.symbol_rate_min	= 870000,
 	.symbol_rate_max	= 6900000,
-	.caps = FE_CAN_INVERSION_AUTO | FE_CAN_QAM_16 | FE_CAN_QAM_32 |
-		FE_CAN_QAM_64 | FE_CAN_QAM_128 | FE_CAN_QAM_256 |
-		FE_CAN_QAM_AUTO,
+
+	.caps 			= FE_CAN_INVERSION_AUTO 	|
+				  FE_CAN_QAM_16			|
+				  FE_CAN_QAM_32			|
+				  FE_CAN_QAM_64			|
+				  FE_CAN_QAM_128		|
+				  FE_CAN_QAM_256		|
+				  FE_CAN_QAM_AUTO,
 };
 
 static struct dvb_frontend_info firesat_T_frontend_info = {
+
 	.name			= "FireSAT DVB-T Frontend",
 	.type			= FE_OFDM,
+
 	.frequency_min		= 49000000,
 	.frequency_max		= 861000000,
 	.frequency_stepsize	= 62500,
-	.caps = FE_CAN_INVERSION_AUTO | FE_CAN_FEC_2_3 |
-		FE_CAN_TRANSMISSION_MODE_AUTO | FE_CAN_GUARD_INTERVAL_AUTO |
-		FE_CAN_HIERARCHY_AUTO,
+
+	.caps 			= FE_CAN_INVERSION_AUTO		|
+				  FE_CAN_FEC_2_3		|
+				  FE_CAN_TRANSMISSION_MODE_AUTO | FE_CAN_GUARD_INTERVAL_AUTO	|
+				  FE_CAN_HIERARCHY_AUTO,
 };
 
 static void firesat_add_host (struct hpsb_host *host) {
