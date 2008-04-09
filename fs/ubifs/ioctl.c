4cc83fb60fc6400cb95f6dfd979a860724b0a1c5
Adrian Hunter <ext-adrian.hunter@nokia.com>
2008-04-09 14:07:52 +0300

    UBIFS: use time granularity consistently
    Signed-off-by: Adrian Hunter <ext-adrian.hunter@nokia.com>

--- fs/ubifs/ioctl.c
+++ fs/ubifs/ioctl.c
@@ -128,7 +128,7 @@ static int setflags(struct inode *inode, int flags)
 	ui->flags = ioctl2ubifs(flags);
 	ubifs_set_inode_flags(inode);
 
-	inode->i_ctime = CURRENT_TIME;
+	inode->i_ctime = ubifs_current_time(inode);
 	mark_inode_dirty_sync(inode);
 
 	ubifs_release_ino_dirty(c, inode, &req);
