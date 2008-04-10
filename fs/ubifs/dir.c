f2e30818efc382dad40a6361157198f888cfbe54
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-04-10 13:16:43 +0300

    UBIFS: fix and amend debugging stuff
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/dir.c
+++ fs/ubifs/dir.c
@@ -372,7 +372,7 @@ static int ubifs_readdir(struct file *filp, void *dirent, filldir_t filldir)
 		ubifs_assert(dent->ch.sqnum > ubifs_inode(dir)->creat_sqnum);
 
 		dbg_gen("feed '%s', ino %llu, new f_pos %#x",
-			dent->name, be64_to_cpu(dent->inum),
+			dent->name, le64_to_cpu(dent->inum),
 			key_hash_flash(c, &dent->key));
 		over = filldir(dirent, dent->name,
 			       le16_to_cpu(dent->nlen), filp->f_pos,
@@ -407,7 +407,7 @@ static int ubifs_readdir(struct file *filp, void *dirent, filldir_t filldir)
 
 		ubifs_assert(dent->ch.sqnum > ubifs_inode(dir)->creat_sqnum);
 		dbg_gen("feed '%s', ino %llu, new f_pos %#x",
-			dent->name, be64_to_cpu(dent->inum),
+			dent->name, le64_to_cpu(dent->inum),
 			key_hash_flash(c, &dent->key));
 
 		over = filldir(dirent, dent->name, le16_to_cpu(dent->nlen),
