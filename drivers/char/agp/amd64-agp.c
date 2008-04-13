259530c0c7b2e83c20171e2ff57e7aa0c2488391
Ingo Molnar <mingo@elte.hu>
2008-04-13 19:24:03 +0200

    x86: sanity check gart for buggy device, fix
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/char/agp/amd64-agp.c
+++ drivers/char/agp/amd64-agp.c
@@ -317,7 +317,7 @@ static __devinit int fix_northbridge(struct pci_dev *nb, struct pci_dev *agp,
 	 * On some sick chips APSIZE is 0. This means it wants 4G
 	 * so let double check that order, and lets trust the AMD NB settings
 	 */
-	if (aper + (32UL<<(20 + order)) > 0x100000000UL) {
+	if (aper + (32ULL<<(20 + order)) > 0x100000000ULL) {
 		printk(KERN_INFO "Aperture size %u MB is not right, using settings from NB\n",
 				  32 << order);
 		order = nb_order;
