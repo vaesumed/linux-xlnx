113c3d6ff70199224c5651270d4df7f9663148a1
Stephen Rothwell <sfr@canb.auug.org.au>
2008-04-16 19:13:05 +1000

    Revert "DVB: add firesat driver"
    This reverts commit f68514e15c4070f1e005503ac2b97fe964442654.

--- drivers/media/dvb/firesat/cmp.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FIRESAT__CMP_H_
-#define __FIRESAT__CMP_H_
-
-#include "firesat.h"
-
-extern int try_CMPEstablishPPconnection(struct firesat *firesat, int output_plug, int iso_channel);
-extern int try_CMPBreakPPconnection(struct firesat *firesat, int output_plug,int iso_channel);
-
-#endif
