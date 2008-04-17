78c3d305e67a96a4c36efdeb17ea2ec3b2b3bc43
Ingo Molnar <mingo@elte.hu>
2008-04-17 21:20:43 +0200

    kmemcheck: fix build
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/kmemcheck.h
+++ include/linux/kmemcheck.h
@@ -1,6 +1,8 @@
 #ifndef LINUX_KMEMCHECK_H
 #define LINUX_KMEMCHECK_H
 
+#include <linux/types.h>
+
 #ifdef CONFIG_KMEMCHECK
 extern int kmemcheck_enabled;
 
@@ -24,6 +26,7 @@ void kmemcheck_mark_uninitialized_pages(struct page *p, unsigned int n);
 #ifndef CONFIG_KMEMCHECK
 #define kmemcheck_enabled 0
 static inline void kmemcheck_init(void) { }
+static inline bool kmemcheck_page_is_tracked(struct page *p) { return false; }
 #endif /* CONFIG_KMEMCHECK */
 
 #endif /* LINUX_KMEMCHECK_H */
