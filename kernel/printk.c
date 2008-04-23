070eac766b32fb36c346e6fb5b108ce904779a02
Ingo Molnar <mingo@elte.hu>
2008-04-23 15:03:13 +0200

    namespacecheck: fixes core
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/printk.c
+++ kernel/printk.c
@@ -228,7 +228,7 @@ static inline void boot_delay_msec(void)
 /*
  * Return the number of unread characters in the log buffer.
  */
-int log_buf_get_len(void)
+static int log_buf_get_len(void)
 {
 	return logged_chars;
 }
@@ -267,7 +267,7 @@ int log_buf_copy(char *dest, int idx, int len)
 /*
  * Extract a single character from the log buffer.
  */
-int log_buf_read(int idx)
+static int log_buf_read(int idx)
 {
 	char ret;
 
