b1fabcabed4c83db33f787b2f2d325012bc9b462
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2008-04-24 11:03:57 -0400

    LTTng instrumentation fs
    Core filesystem events markers.
    
    Markers added :
    
    fs_buffer_wait_end
    fs_buffer_wait_start
    fs_close
    fs_exec
    fs_ioctl
    fs_llseek
    fs_lseek
    fs_open
    fs_pollfd
    fs_pread64
    fs_pwrite64
    fs_read
    fs_readv
    fs_select
    fs_write
    fs_writev
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    CC: Alexander Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- fs/compat.c
+++ fs/compat.c
@@ -50,6 +50,7 @@
 #include <linux/poll.h>
 #include <linux/mm.h>
 #include <linux/eventpoll.h>
+#include <linux/marker.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1401,6 +1402,7 @@ int compat_do_execve(char * filename,
 
 	retval = search_binary_handler(bprm, regs);
 	if (retval >= 0) {
+		trace_mark(fs_exec, "filename %s", filename);
 		/* execve success */
 		security_bprm_free(bprm);
 		acct_update_integrals(current);
--- fs/select.c
+++ fs/select.c
@@ -23,6 +23,7 @@
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/rcupdate.h>
+#include <linux/marker.h>
 
 #include <asm/uaccess.h>
 
@@ -231,6 +232,9 @@ int do_select(int n, fd_set_bits *fds, s64 *timeout)
 				file = fget_light(i, &fput_needed);
 				if (file) {
 					f_op = file->f_op;
+					trace_mark(fs_select,
+							"fd %d timeout #8d%lld",
+							i, (long long)*timeout);
 					mask = DEFAULT_POLLMASK;
 					if (f_op && f_op->poll)
 						mask = (*f_op->poll)(file, retval ? NULL : wait);
@@ -559,6 +563,7 @@ static inline unsigned int do_pollfd(struct pollfd *pollfd, poll_table *pwait)
 		file = fget_light(fd, &fput_needed);
 		mask = POLLNVAL;
 		if (file != NULL) {
+			trace_mark(fs_pollfd, "fd %d", fd);
 			mask = DEFAULT_POLLMASK;
 			if (file->f_op && file->f_op->poll)
 				mask = file->f_op->poll(file, pwait);
