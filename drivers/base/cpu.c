c223930bdc67b010dc103fc9bbae41cf99559c9f
Mike Travis <travis@sgi.com>
2008-04-25 17:15:54 -0700

    cpu: change some globals to statics in drivers/base/cpu.c v2
    This patch makes the following needlessly global code static:
    - attr_online_map
    - attr_possible_map
    - attr_present_map
    
    - cpu_state_attr [v2]
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Mike Travis <travis@sgi.com>
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -119,7 +119,7 @@ static ssize_t print_cpus_##type(struct sysdev_class *class, char *buf)	\
 {									\
 	return print_cpus_map(buf, &cpu_##type##_map);			\
 }									\
-struct sysdev_class_attribute attr_##type##_map = 			\
+static struct sysdev_class_attribute attr_##type##_map = 		\
 	_SYSDEV_CLASS_ATTR(type, 0444, print_cpus_##type, NULL)
 
 print_cpus_func(online);
@@ -127,7 +127,7 @@ print_cpus_func(possible);
 print_cpus_func(present);
 print_cpus_func(system);
 
-struct sysdev_class_attribute *cpu_state_attr[] = {
+static struct sysdev_class_attribute *cpu_state_attr[] = {
 	&attr_online_map,
 	&attr_possible_map,
 	&attr_present_map,
