b75d0c6d33b67857681489d9ea36486409edbbd2
Mike Travis <travis@sgi.com>
2008-04-25 17:15:51 -0700

    x86: restore pda nodenumber field
      * Restore the nodenumber field in the x86_64 pda.  This field is slightly
        different than the x86_cpu_to_node_map mainly because it's a static
        indication of which node the cpu is on while the cpu to node map is a
        dyanamic mapping that may get reset if the cpu goes offline.  This also
        simplifies the numa_node_id() macro.
    
    For inclusion into sched-devel/latest tree.
    
    Based on:
    	git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6.git
        +   sched-devel/latest  .../mingo/linux-2.6-sched-devel.git
    
    Signed-off-by: Mike Travis <travis@sgi.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/pda.h
+++ include/asm-x86/pda.h
@@ -20,6 +20,7 @@ struct x8664_pda {
 					/* gcc-ABI: this canary MUST be at
 					   offset 40!!! */
 	char *irqstackptr;
+	int nodenumber;			/* number of current node */
 	unsigned int __softirq_pending;
 	unsigned int __nmi_count;	/* number of NMI on this CPUs */
 	short mmu_state;
