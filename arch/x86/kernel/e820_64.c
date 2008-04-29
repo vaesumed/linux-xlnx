939979f9fcad1e51a56d2e0105d160b7851af703
Yinghai Lu <yhlu.kernel.send@gmail.com>
2008-04-29 01:59:49 -0700

    x86: fix trimming e820 with MTRR holes.
    converting MTRR layout from continous to discrete, some time could run out of
    MTRRs. So add gran_sizek to prevent that by dumpping small RAM piece less than
    gran_sizek.
    
    previous trimming only can handle highest_pfn from mtrr to end_pfn from e820.
    when have more than 4g RAM installed, there will be holes below 4g. so need to
    check ram below 4g is coverred well.
    
    need to be applied after
    	[PATCH] x86: mtrr cleanup for converting continuous to discrete layout v7
    
    Signed-off-by: Yinghai Lu <yinghai.lu@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/e820_64.c
+++ arch/x86/kernel/e820_64.c
@@ -830,10 +830,11 @@ void __init finish_e820_parsing(void)
 	}
 }
 
-void __init update_memory_range(u64 start, u64 size, unsigned old_type,
+u64 __init update_memory_range(u64 start, u64 size, unsigned old_type,
 				unsigned new_type)
 {
 	int i;
+	u64 real_updated_size = 0;
 
 	BUG_ON(old_type == new_type);
 
@@ -845,6 +846,7 @@ void __init update_memory_range(u64 start, u64 size, unsigned old_type,
 		/* totally covered? */
 		if (ei->addr >= start && ei->size <= size) {
 			ei->type = new_type;
+			real_updated_size += ei->size;
 			continue;
 		}
 		/* partially covered */
@@ -854,7 +856,9 @@ void __init update_memory_range(u64 start, u64 size, unsigned old_type,
 			continue;
 		add_memory_region(final_start, final_end - final_start,
 					 new_type);
+		real_updated_size += final_end - final_start;
 	}
+	return real_updated_size;
 }
 
 void __init update_e820(void)
