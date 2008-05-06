4a2823286e6d195be1d6f7c8e0c53c902c7dd509
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-05-06 10:04:54 +1000

    ide: use __generic_unplug_device() in ide_do_drive_cmd()
    Call __elv_add_request() with 'plug' == 1 (so the device will be
    plugged) and then use __generic_unplug_device() instead of calling
    ide_do_request() directly.
    
    This is a preparation for converting IDE to use blk_execute_rq().
    
    Cc: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Cc: Borislav Petkov <petkovbb@gmail.com>
    Cc: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-io.c
+++ drivers/ide/ide-io.c
@@ -1606,8 +1606,8 @@ int ide_do_drive_cmd (ide_drive_t *drive, struct request *rq, ide_action_t actio
 	spin_lock_irqsave(&ide_lock, flags);
 	if (action == ide_preempt)
 		hwgroup->rq = NULL;
-	__elv_add_request(drive->queue, rq, where, 0);
-	ide_do_request(hwgroup, IDE_NO_IRQ);
+	__elv_add_request(drive->queue, rq, where, 1);
+	__generic_unplug_device(drive->queue);
 	spin_unlock_irqrestore(&ide_lock, flags);
 
 	err = 0;
