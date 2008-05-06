d0e8a74f06263e94b1ff37a8c40b73a87673ccee
Adrian Hunter <ext-adrian.hunter@nokia.com>
2008-05-06 16:19:22 +0300

    UBIFS: add warning for mmapped file
    Signed-off-by: Adrian Hunter <ext-adrian.hunter@nokia.com>

--- fs/ubifs/file.c
+++ fs/ubifs/file.c
@@ -806,8 +806,12 @@ static int ubifs_vm_page_mkwrite(struct vm_area_struct *vma,struct page *page)
 		update_time = 0;
 		err = ubifs_budget_space(c, &req);
 	}
-	if (unlikely(err))
+	if (unlikely(err)) {
+		if (err == -ENOSPC)
+			ubifs_warn("out of space for mmapped file "
+				   "(inode number %lu)", inode->i_ino);
 		return err;
+	}
 
 	lock_page(page);
 	if (unlikely(page->mapping != inode->i_mapping ||
