b4e6d233104e32018d12c4cf87d7080868a8c22a
Rusty Russell <rusty@rustcorp.com.au>
2008-05-23 10:07:59 +1000

    misc:down_nowait-xfs
    down_trylock -> down_try in xfs
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Cc: xfs@oss.sgi.com

--- fs/xfs/linux-2.6/xfs_buf.c
+++ fs/xfs/linux-2.6/xfs_buf.c
@@ -530,7 +530,7 @@ found:
 	 * if this does not work then we need to drop the
 	 * spinlock and do a hard attempt on the semaphore.
 	 */
-	if (down_trylock(&bp->b_sema)) {
+	if (!down_try(&bp->b_sema)) {
 		if (!(flags & XBF_TRYLOCK)) {
 			/* wait for buffer ownership */
 			XB_TRACE(bp, "get_lock", 0);
@@ -873,7 +873,7 @@ xfs_buf_cond_lock(
 {
 	int			locked;
 
-	locked = down_trylock(&bp->b_sema) == 0;
+	locked = down_try(&bp->b_sema);
 	if (locked) {
 		XB_SET_OWNER(bp);
 	}
