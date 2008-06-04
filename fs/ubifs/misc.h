33f3790e442e42fc01d70a11e2b0c791b43a4ee7
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-06-04 12:43:01 +0300

    UBIFS: rename NC to LPROPS_NC
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- fs/ubifs/misc.h
+++ fs/ubifs/misc.h
@@ -212,7 +212,7 @@ static inline int ubifs_encode_dev(union ubifs_dev_desc *dev, dev_t rdev)
  */
 static inline int ubifs_add_dirt(struct ubifs_info *c, int lnum, int dirty)
 {
-	return ubifs_update_one_lp(c, lnum, NC, dirty, 0, 0);
+	return ubifs_update_one_lp(c, lnum, LPROPS_NC, dirty, 0, 0);
 }
 
 /**
@@ -226,7 +226,8 @@ static inline int ubifs_add_dirt(struct ubifs_info *c, int lnum, int dirty)
  */
 static inline int ubifs_return_leb(struct ubifs_info *c, int lnum)
 {
-	return ubifs_change_one_lp(c, lnum, NC, NC, 0, LPROPS_TAKEN, 0);
+	return ubifs_change_one_lp(c, lnum, LPROPS_NC, LPROPS_NC, 0,
+				   LPROPS_TAKEN, 0);
 }
 
 /**
