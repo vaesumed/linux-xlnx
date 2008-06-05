76256b70096015296579b73a3cacf186a2193e60
Adrian Bunk <bunk@kernel.org>
2008-06-05 11:44:39 -0700

    drivers/sbus/: cleanups
    This patch contains the following cleanups:
    - make the following needlessly global code static:
      - char/uctrl.c: uctrl_get_event_status()
      - char/uctrl.c: uctrl_get_external_status()
      - char/vfc_dev.c: struct vfc_dev_lst
      - char/vfc_dev.c: vfc_lock_device()
      - char/vfc_dev.c: vfc_unlock_device()
      - char/vfc_dev.c: vfc_captstat_reset()
      - char/vfc_dev.c: vfc_memptr_reset()
      - char/vfc_dev.c: vfc_csr_init()
      - char/vfc_dev.c: vfc_saa9051_init()
      - char/vfc_dev.c: init_vfc_hw()
      - char/vfc_dev.c: init_vfc_devstruct()
      - char/vfc_dev.c: init_vfc_device()
      - char/vfc_dev.c: vfc_get_dev_ptr()
      - char/vfc_dev.c: vfc_capture_start()
      - char/vfc_dev.c: vfc_capture_poll()
      - char/vfc_dev.c: vfc_port_change_ioctl()
      - char/vfc_dev.c: vfc_set_video_ioctl()
      - char/vfc_dev.c: vfc_get_video_ioctl()
      - char/vfc_i2c.c: vfc_i2c_wait_for_bus()
      - char/vfc_i2c.c: vfc_i2c_wait_for_pin()
      - char/vfc_i2c.c: vfc_i2c_xmit_addr()
      - char/vfc_i2c.c: vfc_i2c_xmit_byte()
      - char/vfc_i2c.c: vfc_i2c_recv_byte()
      - dvma.c: init_one_dvma()
    - remove an unused variable from a function:
      - char/uctrl.c: ts102_uctrl_init()
    - remove the following unused and empty function:
      - char/uctrl.c: uctrl_set_video()
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/sbus/char/uctrl.c
+++ drivers/sbus/char/uctrl.c
@@ -194,8 +194,8 @@ struct uctrl_driver {
 
 static struct uctrl_driver drv;
 
-void uctrl_get_event_status(void);
-void uctrl_get_external_status(void);
+static void uctrl_get_event_status(void);
+static void uctrl_get_external_status(void);
 
 static int
 uctrl_ioctl(struct inode *inode, struct file *file,
@@ -263,12 +263,6 @@ static struct miscdevice uctrl_dev = {
   driver->regs->uctrl_stat = UCTRL_STAT_RXNE_STA; \
 }
 
-void uctrl_set_video(int status)
-{
-	struct uctrl_driver *driver = &drv;
-	
-}
-
 static void uctrl_do_txn(struct uctrl_txn *txn)
 {
 	struct uctrl_driver *driver = &drv;
@@ -308,7 +302,7 @@ static void uctrl_do_txn(struct uctrl_txn *txn)
 	}
 }
 
-void uctrl_get_event_status(void)
+static void uctrl_get_event_status(void)
 {
 	struct uctrl_driver *driver = &drv;
 	struct uctrl_txn txn;
@@ -328,7 +322,7 @@ void uctrl_get_event_status(void)
 	dprintk(("ev is %x\n", driver->status.event_status));
 }
 
-void uctrl_get_external_status(void)
+static void uctrl_get_external_status(void)
 {
 	struct uctrl_driver *driver = &drv;
 	struct uctrl_txn txn;
@@ -360,7 +354,7 @@ void uctrl_get_external_status(void)
 static int __init ts102_uctrl_init(void)
 {
 	struct uctrl_driver *driver = &drv;
-	int len, i;
+	int len;
 	struct linux_prom_irqs tmp_irq[2];
         unsigned int vaddr[2] = { 0, 0 };
 	int tmpnode, uctrlnode = prom_getchild(prom_root_node);
