7ffd548201f7df1ce50a1d7bc7aba3b0b083927c
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-06-19 09:33:36 +1000

    ide: move ide_pio_cycle_time() to ide-timings.c
    All ide_pio_cycle_time() users already select CONFIG_IDE_TIMINGS
    so move the function from ide-lib.c to ide-timings.c.
    
    While at it:
    
    - convert ide_pio_cycle_time() to use ide_timing_find_mode()
    
    - cleanup ide_pio_cycle_time() a bit
    
    There should be no functional changes caused by this patch.
    
    Acked-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- include/linux/ide.h
+++ include/linux/ide.h
@@ -1285,6 +1285,7 @@ enum {
 };
 
 struct ide_timing *ide_timing_find_mode(u8);
+u16 ide_pio_cycle_time(ide_drive_t *, u8);
 void ide_timing_merge(struct ide_timing *, struct ide_timing *,
 		      struct ide_timing *, unsigned int);
 int ide_timing_compute(ide_drive_t *, u8, struct ide_timing *, int, int);
@@ -1296,7 +1297,6 @@ typedef struct ide_pio_timings_s {
 				/* active + recovery (+ setup for some chips) */
 } ide_pio_timings_t;
 
-unsigned int ide_pio_cycle_time(ide_drive_t *, u8);
 u8 ide_get_best_pio_mode(ide_drive_t *, u8, u8);
 extern const ide_pio_timings_t ide_pio_timings[6];
 
