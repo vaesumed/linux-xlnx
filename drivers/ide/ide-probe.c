02c6ffc8279b5e3e2189fb646bdcdfb574c67146
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-07-08 09:56:37 +1000

    ide: print message on error in ide_find_port_slot()
    * Add DRV_NAME define to ide-h8300.c.
    
    * Fix ide-h8300.c, swarm.c and sgiioc4.c to set .name field in
      struct ide_port_info to DRV_NAME, then convert these host drivers
      to use ide_find_port_slot() instead of ide_find_port().
    
    * Print message on error in ide_find_port_slot().
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-probe.c
+++ drivers/ide/ide-probe.c
@@ -1494,6 +1494,9 @@ ide_hwif_t *ide_find_port_slot(const struct ide_port_info *d)
 		}
 	}
 
+	printk(KERN_ERR "%s: no free slot for interface\n",
+			d ? d->name : "ide");
+
 	return NULL;
 
 out_found:
