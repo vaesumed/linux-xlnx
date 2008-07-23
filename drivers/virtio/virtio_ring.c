c0bbe61256cb4fd51e61b7e9dfc223c92c1fc00b
Rusty Russell <rusty@rustcorp.com.au>
2008-07-23 10:07:05 +1000

    virtio:vring-feature-handling
    To prepare for virtio_ring transport feature bits, hook in a call in
    all the users to manipulate them.  This currently just clears all the
    bits, since it doesn't understand any features.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -323,4 +323,19 @@ void vring_del_virtqueue(struct virtqueue *vq)
 }
 EXPORT_SYMBOL_GPL(vring_del_virtqueue);
 
+/* Manipulates transport-specific feature bits. */
+void vring_transport_features(struct virtio_device *vdev)
+{
+	unsigned int i;
+
+	for (i = VIRTIO_TRANSPORT_F_START; i < VIRTIO_TRANSPORT_F_END; i++) {
+		switch (i) {
+		default:
+			/* We don't understand this bit. */
+			clear_bit(i, vdev->features);
+		}
+	}
+}
+EXPORT_SYMBOL_GPL(vring_transport_features);
+
 MODULE_LICENSE("GPL");
