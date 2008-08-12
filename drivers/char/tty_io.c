dec07338da3a1b55ebeb4c4f85f02fe135e77b22
Alan Cox <alan@redhat.com>
2008-08-13 09:55:42 +1000

    tty-current-fix-abusers
    Various people outside the tty layer still stick their noses in behind the
    scenes. We need to stop that.

--- drivers/char/tty_io.c
+++ drivers/char/tty_io.c
@@ -3174,9 +3174,9 @@ struct tty_struct *get_current_tty(void)
 	struct tty_struct *tty;
 	unsigned long flags;
 
-	spin_lock_irqsave(&tsk->sighand->siglock, flags);
+	spin_lock_irqsave(&current->sighand->siglock, flags);
 	tty = tty_kref_get(current->signal->tty);
-	spin_unlock_irqrestore(&tsk->sighand->siglock, flags);
+	spin_unlock_irqrestore(&current->sighand->siglock, flags);
 	return tty;
 }
 EXPORT_SYMBOL_GPL(get_current_tty);
