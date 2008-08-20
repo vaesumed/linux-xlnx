5dfa07fb6db34c26e5711425b391e3092c139029
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-08-20 10:35:34 +1000

    ide: add proper PCI PM support
    * Keep pointer to ->init_chipset method also in
      struct ide_host and set it in ide_host_alloc_all().
    
    * Add ide_pci_suspend() and ide_pci_resume() helpers
      (default ->suspend and ->resume implementations).
    
    * ->init_chipset can no longer be marked __devinit.
    
    * Add proper PCI PM support to IDE PCI host drivers
      (rz1000.c and tc86c001.c are skipped for now since
      they need to be converted from using ->init_hwif
      to use ->init_chipset instead).
    
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/jmicron.c
+++ drivers/ide/pci/jmicron.c
@@ -154,6 +154,10 @@ static struct pci_driver driver = {
 	.id_table	= jmicron_pci_tbl,
 	.probe		= jmicron_init_one,
 	.remove		= ide_pci_remove,
+#ifdef CONFIG_PM
+	.suspend	= ide_pci_suspend,
+	.resume		= ide_pci_resume,
+#endif
 };
 
 static int __init jmicron_ide_init(void)
