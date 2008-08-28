8a89d640448c261c248594511c72db0569725058
Jason Wessel <jason.wessel@windriver.com>
2008-08-28 11:37:58 -0500

    kgdboc,tty: Fix tty polling search to use name correctly
    The tty_find_polling_driver() routine did not correctly check the base
    part of the tty name.  This can lead to kgdboc selecting an incorrect
    driver, as well as accepting a completely invalid tty such as "echo
    ffff0 > /sys/module/kgdboc/parameters/kgdboc".
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- drivers/char/tty_io.c
+++ drivers/char/tty_io.c
@@ -695,20 +695,31 @@ struct tty_driver *tty_find_polling_driver(char *name, int *line)
 {
 	struct tty_driver *p, *res = NULL;
 	int tty_line = 0;
+	int len;
 	char *str;
+
+	for (str = name; *str; str++)
+		if ((*str >= '0' && *str <= '9') || *str == ',')
+			break;
+	if (!*str)
+		return NULL;
+
+	len = str - name;
+	tty_line = strict_strtoul(str, &str, 10);
 
 	mutex_lock(&tty_mutex);
 	/* Search through the tty devices to look for a match */
 	list_for_each_entry(p, &tty_drivers, tty_drivers) {
-		str = name + strlen(p->name);
-		tty_line = simple_strtoul(str, &str, 10);
+		if (strncmp(name, p->name, len) != 0)
+			continue;
 		if (*str == ',')
 			str++;
 		if (*str == '\0')
 			str = NULL;
 
 		if (tty_line >= 0 && tty_line <= p->num && p->ops &&
-		    p->ops->poll_init && !p->ops->poll_init(p, tty_line, str)) {
+		    p->ops->poll_init &&
+			!p->ops->poll_init(p, tty_line, str)) {
 			res = p;
 			*line = tty_line;
 			break;
