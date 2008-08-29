f202dbc68cba797963cc449736bf78e9076e3793
Jens Axboe <jens.axboe@oracle.com>
2008-08-29 09:34:21 +0200

    block: unify request timeout handling
    Right now SCSI and others do their own command timeout handling.
    Move those bits to the block layer.
    
    Signed-off-by: Mike Anderson <andmike@linux.vnet.ibm.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- block/blk.h
+++ block/blk.h
@@ -17,6 +17,7 @@ void __blk_queue_free_tags(struct request_queue *q);
 
 void blk_unplug_work(struct work_struct *work);
 void blk_unplug_timeout(unsigned long data);
+void blk_rq_timed_out_timer(unsigned long data);
 
 struct io_context *current_io_context(gfp_t gfp_flags, int node);
 
