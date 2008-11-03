6d662088c50fc802aa41a69de439012ec878b4e0
Christof Schmitt <christof.schmitt@de.ibm.com>
2008-11-03 16:09:33 +0100

    [S390] convert zfcp printks to pr_xxx macros.
    Signed-off-by: Christof Schmitt <christof.schmitt@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/scsi/zfcp_aux.c
+++ drivers/s390/scsi/zfcp_aux.c
@@ -25,6 +25,8 @@
  *            Sven Schuetz
  */
 
+#define KMSG_COMPONENT "zfcp"
+
 #include <linux/miscdevice.h>
 #include "zfcp_ext.h"
 
@@ -104,7 +106,7 @@ static int __init zfcp_device_setup(char *devstr)
 
  err_out:
 	kfree(str);
-	pr_err("zfcp: %s is not a valid SCSI device\n", devstr);
+	pr_err("%s is not a valid SCSI device\n", devstr);
 	return 0;
 }
 
@@ -188,13 +190,13 @@ static int __init zfcp_module_init(void)
 
 	retval = misc_register(&zfcp_cfdc_misc);
 	if (retval) {
-		pr_err("zfcp: Registering the misc device zfcp_cfdc failed\n");
+		pr_err("Registering the misc device zfcp_cfdc failed\n");
 		goto out_misc;
 	}
 
 	retval = zfcp_ccw_register();
 	if (retval) {
-		pr_err("zfcp: The zfcp device driver could not register with "
+		pr_err("The zfcp device driver could not register with "
 		       "the common I/O layer\n");
 		goto out_ccw_register;
 	}
