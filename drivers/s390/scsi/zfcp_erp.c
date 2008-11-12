4389d263cb4391ca9b0187a335204e1e307b4746
Christof Schmitt <christof.schmitt@de.ibm.com>
2008-11-12 14:55:49 +0100

    [S390] zfcp: Report microcode level through service level interface
    Register zfcp with the new /proc/service_level interface to report the
    FCP microcode level. When the adapter goes offline or a channel path
    disappears, zfcp unregisters, since the microcode version might change
    and zfcp does not know about it.
    
    Signed-off-by: Christof Schmitt <christof.schmitt@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/scsi/zfcp_erp.c
+++ drivers/s390/scsi/zfcp_erp.c
@@ -1279,8 +1279,11 @@ static void zfcp_erp_action_cleanup(struct zfcp_erp_action *act, int result)
 		break;
 
 	case ZFCP_ERP_ACTION_REOPEN_ADAPTER:
-		if (result != ZFCP_ERP_SUCCEEDED)
+		if (result != ZFCP_ERP_SUCCEEDED) {
+			unregister_service_level(&adapter->service_level);
 			zfcp_erp_rports_del(adapter);
+		} else
+			register_service_level(&adapter->service_level);
 		zfcp_adapter_put(adapter);
 		break;
 	}
