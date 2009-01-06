9f7c04ec9d0b46e91729d9e0f05eb57627bfd509
Eric Miao <eric.miao@marvell.com>
2009-01-06 18:29:01 +0800

    [ARM] pxa: move GPIO register definitions into <mach/gpio.h>
    This makes gpio.c fully independent of pxa-regs.h (except for the
    virtual address of the registers).
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>

--- arch/arm/mach-pxa/include/mach/gpio.h
+++ arch/arm/mach-pxa/include/mach/gpio.h
@@ -24,12 +24,77 @@
 #ifndef __ASM_ARCH_PXA_GPIO_H
 #define __ASM_ARCH_PXA_GPIO_H
 
-#include <mach/pxa-regs.h>
-#include <asm/irq.h>
+#include <mach/irqs.h>
 #include <mach/hardware.h>
-
 #include <asm-generic/gpio.h>
 
+#define GPIO_REGS_VIRT	io_p2v(0x40E00000)
+#define GPIO_REG(x)	(*((volatile u32 *)(GPIO_REGS_VIRT + (x))))
+
+/* GPIO Pin Level Registers */
+#define GPLR0		GPIO_REG(0x0000)
+#define GPLR1		GPIO_REG(0x0004)
+#define GPLR2		GPIO_REG(0x0008)
+#define GPLR3		GPIO_REG(0x0100)
+
+/* GPIO Pin Direction Registers */
+#define GPDR0		GPIO_REG(0x000C)
+#define GPDR1		GPIO_REG(0x0010)
+#define GPDR2		GPIO_REG(0x0014)
+#define GPDR3		GPIO_REG(0x010C)
+
+/* GPIO Pin Output Set Registers */
+#define GPSR0		GPIO_REG(0x0018)
+#define GPSR1		GPIO_REG(0x001C)
+#define GPSR2		GPIO_REG(0x0020)
+#define GPSR3		GPIO_REG(0x0118)
+
+/* GPIO Pin Output Clear Registers */
+#define GPCR0		GPIO_REG(0x0024)
+#define GPCR1		GPIO_REG(0x0028)
+#define GPCR2		GPIO_REG(0x002C)
+#define GPCR3		GPIO_REG(0x0124)
+
+/* GPIO Rising Edge Detect Registers */
+#define GRER0		GPIO_REG(0x0030)
+#define GRER1		GPIO_REG(0x0034)
+#define GRER2		GPIO_REG(0x0038)
+#define GRER3		GPIO_REG(0x0130)
+
+/* GPIO Falling Edge Detect Registers */
+#define GFER0		GPIO_REG(0x003C)
+#define GFER1		GPIO_REG(0x0040)
+#define GFER2		GPIO_REG(0x0044)
+#define GFER3		GPIO_REG(0x013C)
+
+/* GPIO Edge Detect Status Registers */
+#define GEDR0		GPIO_REG(0x0048)
+#define GEDR1		GPIO_REG(0x004C)
+#define GEDR2		GPIO_REG(0x0050)
+#define GEDR3		GPIO_REG(0x0148)
+
+/* GPIO Alternate Function Select Registers */
+#define GAFR0_L		GPIO_REG(0x0054)
+#define GAFR0_U		GPIO_REG(0x0058)
+#define GAFR1_L		GPIO_REG(0x005C)
+#define GAFR1_U		GPIO_REG(0x0060)
+#define GAFR2_L		GPIO_REG(0x0064)
+#define GAFR2_U		GPIO_REG(0x0068)
+#define GAFR3_L		GPIO_REG(0x006C)
+#define GAFR3_U		GPIO_REG(0x0070)
+
+/* More handy macros.  The argument is a literal GPIO number. */
+
+#define GPIO_bit(x)	(1 << ((x) & 0x1f))
+
+#define GPLR(x)		GPIO_REG((((x) < 96) ? 0x000 : 0x100) + (((x) & 0x60) >> 3))
+#define GPDR(x)		GPIO_REG((((x) < 96) ? 0x00c : 0x10c) + (((x) & 0x60) >> 3))
+#define GPSR(x)		GPIO_REG((((x) < 96) ? 0x018 : 0x118) + (((x) & 0x60) >> 3))
+#define GPCR(x)		GPIO_REG((((x) < 96) ? 0x024 : 0x124) + (((x) & 0x60) >> 3))
+#define GRER(x)		GPIO_REG((((x) < 96) ? 0x030 : 0x130) + (((x) & 0x60) >> 3))
+#define GFER(x)		GPIO_REG((((x) < 96) ? 0x03c : 0x13c) + (((x) & 0x60) >> 3))
+#define GEDR(x)		GPIO_REG((((x) < 96) ? 0x048 : 0x148) + (((x) & 0x60) >> 3))
+#define GAFR(x)		GPIO_REG(0x054 + (((x) & 0x70) >> 2))
 
 /* NOTE: some PXAs have fewer on-chip GPIOs (like PXA255, with 85).
  * Those cases currently cause holes in the GPIO number space.
