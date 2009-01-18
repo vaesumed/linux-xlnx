915073a21f88b12bcbf43083db2bd73634d7c2cb
Stefan Richter <stefanr@s5r6.in-berlin.de>
2009-01-18 16:30:00 +0100

    firedtv: iso: style changes and fixlets
    Add cleanup after failure in setup_iso_channel.
    Replace printk() by dv_err().
    Decrease indentation level in rawiso_activity_cb().
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/media/dvb/firewire/firedtv-iso.c
+++ drivers/media/dvb/firewire/firedtv-iso.c
@@ -9,6 +9,7 @@
  *	the License, or (at your option) any later version.
  */
 
+#include <linux/device.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
@@ -22,7 +23,53 @@
 
 #include "firedtv.h"
 
-static void rawiso_activity_cb(struct hpsb_iso *iso);
+static void rawiso_activity_cb(struct hpsb_iso *iso)
+{
+	struct firedtv *fdtv_iterator, *fdtv = NULL;
+	unsigned int i, num, packet;
+	unsigned char *buf;
+	unsigned long flags;
+	int count;
+
+	spin_lock_irqsave(&fdtv_list_lock, flags);
+	list_for_each_entry(fdtv_iterator, &fdtv_list, list)
+		if(fdtv_iterator->iso_handle == iso) {
+			fdtv = fdtv_iterator;
+			break;
+		}
+	spin_unlock_irqrestore(&fdtv_list_lock, flags);
+
+	packet = iso->first_packet;
+	num = hpsb_iso_n_ready(iso);
+
+	if (!fdtv) {
+		dev_err(&fdtv->ud->device, "received at unknown iso channel\n");
+		goto out;
+	}
+
+	for (i = 0; i < num; i++, packet = (packet + 1) % iso->buf_packets) {
+		buf = dma_region_i(&iso->data_buf, unsigned char,
+			iso->infos[packet].offset + sizeof(struct CIPHeader));
+		count = (iso->infos[packet].len - sizeof(struct CIPHeader)) /
+			(188 + sizeof(struct firewireheader));
+
+		/* ignore empty packet */
+		if (iso->infos[packet].len <= sizeof(struct CIPHeader))
+			continue;
+
+		while (count--) {
+			if (buf[sizeof(struct firewireheader)] == 0x47)
+				dvb_dmx_swfilter_packets(&fdtv->demux,
+				    &buf[sizeof(struct firewireheader)], 1);
+			else
+				dev_err(&fdtv->ud->device,
+					"skipping invalid packet\n");
+			buf += 188 + sizeof(struct firewireheader);
+		}
+	}
+out:
+	hpsb_iso_recv_release_packets(iso, num);
+}
 
 void tear_down_iso_channel(struct firedtv *fdtv)
 {
@@ -33,79 +80,28 @@ void tear_down_iso_channel(struct firedtv *fdtv)
 	fdtv->iso_handle = NULL;
 }
 
+#define FDTV_ISO_BUFFER_PACKETS 256
+#define FDTV_ISO_BUFFER_SIZE (FDTV_ISO_BUFFER_PACKETS * 200)
+
 int setup_iso_channel(struct firedtv *fdtv)
 {
-	int result;
-	fdtv->iso_handle =
-		hpsb_iso_recv_init(fdtv->ud->ne->host,
-				   256 * 200, //data_buf_size,
-				   256, //buf_packets,
-				   fdtv->isochannel,
-				   HPSB_ISO_DMA_DEFAULT, //dma_mode,
-				   -1, //stat.config.irq_interval,
-				   rawiso_activity_cb);
+	int ret;
+
+	fdtv->iso_handle = hpsb_iso_recv_init(fdtv->ud->ne->host,
+				FDTV_ISO_BUFFER_SIZE, FDTV_ISO_BUFFER_PACKETS,
+				fdtv->isochannel, HPSB_ISO_DMA_DEFAULT,
+				-1, /* stat.config.irq_interval */
+				rawiso_activity_cb);
 	if (fdtv->iso_handle == NULL) {
-		printk(KERN_ERR "Cannot initialize iso receive.\n");
-		return -EINVAL;
+		dev_err(&fdtv->ud->device, "cannot initialize iso receive\n");
+		return -ENOMEM;
 	}
-	result = hpsb_iso_recv_start(fdtv->iso_handle, -1, -1, 0);
-	if (result != 0) {
-		printk(KERN_ERR "Cannot start iso receive.\n");
-		return -EINVAL;
+
+	ret = hpsb_iso_recv_start(fdtv->iso_handle, -1, -1, 0);
+	if (ret != 0) {
+		dev_err(&fdtv->ud->device, "cannot start iso receive\n");
+		hpsb_iso_shutdown(fdtv->iso_handle);
+		fdtv->iso_handle = NULL;
 	}
-	return 0;
+	return ret;
 }
-
-static void rawiso_activity_cb(struct hpsb_iso *iso)
-{
-	unsigned int num;
-	unsigned int i;
-	unsigned int packet;
-	unsigned long flags;
-	struct firedtv *fdtv = NULL;
-	struct firedtv *fdtv_iterator;
-
-	spin_lock_irqsave(&fdtv_list_lock, flags);
-	list_for_each_entry(fdtv_iterator, &fdtv_list, list) {
-		if(fdtv_iterator->iso_handle == iso) {
-			fdtv = fdtv_iterator;
-			break;
-		}
-	}
-	spin_unlock_irqrestore(&fdtv_list_lock, flags);
-
-	if (fdtv) {
-		packet = iso->first_packet;
-		num = hpsb_iso_n_ready(iso);
-		for (i = 0; i < num; i++,
-			     packet = (packet + 1) % iso->buf_packets) {
-			unsigned char *buf =
-				dma_region_i(&iso->data_buf, unsigned char,
-					     iso->infos[packet].offset +
-					     sizeof(struct CIPHeader));
-			int count = (iso->infos[packet].len -
-				     sizeof(struct CIPHeader)) /
-				(188 + sizeof(struct firewireheader));
-			if (iso->infos[packet].len <= sizeof(struct CIPHeader))
-				continue; // ignore empty packet
-
-			while (count --) {
-				if (buf[sizeof(struct firewireheader)] == 0x47)
-					dvb_dmx_swfilter_packets(&fdtv->demux,
-								 &buf[sizeof(struct firewireheader)], 1);
-				else
-					printk("%s: invalid packet, skipping\n", __func__);
-				buf += 188 + sizeof(struct firewireheader);
-
-			}
-
-		}
-		hpsb_iso_recv_release_packets(iso, num);
-	}
-	else {
-		printk("%s: packets for unknown iso channel, skipping\n",
-		       __func__);
-		hpsb_iso_recv_release_packets(iso, hpsb_iso_n_ready(iso));
-	}
-}
-
