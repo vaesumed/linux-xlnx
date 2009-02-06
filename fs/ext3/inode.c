d45eed81be69809255ea6ef3b350f68f59651545
David Howells <dhowells@redhat.com>
2009-02-06 13:11:24 +0000

    CacheFiles: Add a hook to write a single page of data to an inode
    Add an address space operation to write one single page of data to an inode at
    a page-aligned location (thus permitting the implementation to be highly
    optimised).  The data source is a single page.
    
    This is used by CacheFiles to store the contents of netfs pages into their
    backing file pages.
    
    Supply a generic implementation for this that uses the write_begin() and
    write_end() address_space operations to bind a copy directly into the page
    cache.
    
    Hook the Ext2 and Ext3 operations to the generic implementation.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/ext3/inode.c
+++ fs/ext3/inode.c
@@ -1795,6 +1795,7 @@ static const struct address_space_operations ext3_ordered_aops = {
 	.direct_IO		= ext3_direct_IO,
 	.migratepage		= buffer_migrate_page,
 	.is_partially_uptodate  = block_is_partially_uptodate,
+	.write_one_page		= generic_file_buffered_write_one_page,
 };
 
 static const struct address_space_operations ext3_writeback_aops = {
@@ -1810,6 +1811,7 @@ static const struct address_space_operations ext3_writeback_aops = {
 	.direct_IO		= ext3_direct_IO,
 	.migratepage		= buffer_migrate_page,
 	.is_partially_uptodate  = block_is_partially_uptodate,
+	.write_one_page		= generic_file_buffered_write_one_page,
 };
 
 static const struct address_space_operations ext3_journalled_aops = {
@@ -1824,6 +1826,7 @@ static const struct address_space_operations ext3_journalled_aops = {
 	.invalidatepage		= ext3_invalidatepage,
 	.releasepage		= ext3_releasepage,
 	.is_partially_uptodate  = block_is_partially_uptodate,
+	.write_one_page		= generic_file_buffered_write_one_page,
 };
 
 void ext3_set_aops(struct inode *inode)
