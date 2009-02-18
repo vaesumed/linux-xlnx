fa185f178b912f9668f50a9939e5b7b405151e24
Mark Fasheh <mfasheh@suse.com>
2009-02-18 11:41:38 -0800

    ocfs2: re-order ocfs2_empty_dir checks
    ocfs2_empty_dir() is far more expensive than checking link count. Since both
    need to be checked at the same time, we can improve performance by checking
    link count first.
    
    Signed-off-by: Mark Fasheh <mfasheh@suse.com>

--- fs/ocfs2/namei.c
+++ fs/ocfs2/namei.c
@@ -833,10 +833,7 @@ static int ocfs2_unlink(struct inode *dir,
 	child_locked = 1;
 
 	if (S_ISDIR(inode->i_mode)) {
-	       	if (!ocfs2_empty_dir(inode)) {
-			status = -ENOTEMPTY;
-			goto leave;
-		} else if (inode->i_nlink != 2) {
+		if (inode->i_nlink != 2 || !ocfs2_empty_dir(inode)) {
 			status = -ENOTEMPTY;
 			goto leave;
 		}
@@ -1279,8 +1276,8 @@ static int ocfs2_rename(struct inode *old_dir,
 
 	if (target_exists) {
 		if (S_ISDIR(new_inode->i_mode)) {
-			if (!ocfs2_empty_dir(new_inode) ||
-			    new_inode->i_nlink != 2) {
+			if (new_inode->i_nlink != 2 ||
+			    !ocfs2_empty_dir(new_inode)) {
 				status = -ENOTEMPTY;
 				goto bail;
 			}
