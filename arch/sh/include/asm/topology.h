d22e50f66d40a2efad667c840ad09fbc85b47bb9
Rusty Russell <rusty@rustcorp.com.au>
2009-03-10 10:03:53 +1100

    cpumask:remove-pcibus_to_cpumask-sh
    Impact: reduce stack usage for large NR_CPUS
    
    cpumask_of_pcibus() is the new version.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/sh/include/asm/topology.h
+++ arch/sh/include/asm/topology.h
@@ -36,10 +36,6 @@
 #define node_to_first_cpu(node)	((void)(node),0)
 
 #define pcibus_to_node(bus)	((void)(bus), -1)
-#define pcibus_to_cpumask(bus)	(pcibus_to_node(bus) == -1 ? \
-					CPU_MASK_ALL : \
-					node_to_cpumask(pcibus_to_node(bus)) \
-				)
 #define cpumask_of_pcibus(bus)	(pcibus_to_node(bus) == -1 ? \
 				 cpu_all_mask : \
 				 cpumask_of_node(pcibus_to_node(bus)))
