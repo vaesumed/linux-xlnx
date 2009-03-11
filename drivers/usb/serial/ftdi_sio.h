e0aecdc1dfc7133d0ba53a4f37a3da8b379b59a6
Michael Hennerich <michael.hennerich@analog.com>
2009-03-12 10:00:14 +1100

    tty-ftdi-gnice
    Blacklist interface0
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>
    Signed-off-by: Alan Cox <alan@lxorguk.ukuu.org.uk>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -893,6 +893,13 @@
 #define DIEBOLD_BCS_SE923_PID	0xfb99
 
 /*
+ * Blackfin gnICE JTAG
+ * http://docs.blackfin.uclinux.org/doku.php?id=hw:jtag:gnice
+ */
+#define ADI_VID 		0x0456
+#define ADI_GNICE_PID 		0xF000
+
+/*
  *   BmRequestType:  1100 0000b
  *   bRequest:       FTDI_E2_READ
  *   wValue:         0
