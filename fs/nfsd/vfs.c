81e4ed372a32487bd16209312f744e56e3068067
Al Viro <viro@zeniv.linux.org.uk>
2009-04-18 13:58:15 -0400

    switch follow_down()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -104,8 +104,7 @@ nfsd_cross_mnt(struct svc_rqst *rqstp, struct dentry **dpp,
 			    .dentry = dget(dentry)};
 	int err = 0;
 
-	while (follow_down(&path.mnt, &path.dentry) &&
-	       d_mountpoint(path.dentry))
+	while (d_mountpoint(path.dentry) && follow_down(&path))
 		;
 
 	exp2 = rqst_exp_get_by_name(rqstp, &path);
