8be85807f8e3779ac48820b745833975a7ab3036
Greg Kroah-Hartman <gregkh@suse.de>
2009-04-24 14:56:26 -0700

    USB: add usb debugfs directory
    Add a common usb directory in debugfs that the usb subsystem can use.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -1561,6 +1561,9 @@ extern void usb_unregister_notify(struct notifier_block *nb);
 #define err(format, arg...) printk(KERN_ERR KBUILD_MODNAME ": " \
 	format "\n" , ## arg)
 
+/* debugfs stuff */
+extern struct dentry *usb_debug_root;
+
 #endif  /* __KERNEL__ */
 
 #endif
