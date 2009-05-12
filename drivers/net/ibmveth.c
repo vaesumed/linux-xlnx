92c04c8527f9616111650aac71e64346794f0736
Jiri Pirko <jpirko@redhat.com>
2009-05-12 11:37:15 +0200

    net: remove needless (now buggy) & from dev->dev_addr
    Patch fixes issues with dev->dev_addr changing from array to pointer.
    Hopefully there are no others.
    
    Compile tested only.
    
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>

--- drivers/net/ibmveth.c
+++ drivers/net/ibmveth.c
@@ -1368,8 +1368,8 @@ static void ibmveth_proc_unregister_driver(void)
 static int ibmveth_show(struct seq_file *seq, void *v)
 {
 	struct ibmveth_adapter *adapter = seq->private;
-	char *current_mac = ((char*) &adapter->netdev->dev_addr);
-	char *firmware_mac = ((char*) &adapter->mac_addr) ;
+	char *current_mac = (char *) adapter->netdev->dev_addr;
+	char *firmware_mac = (char *) &adapter->mac_addr;
 
 	seq_printf(seq, "%s %s\n\n", ibmveth_driver_string, ibmveth_driver_version);
 
