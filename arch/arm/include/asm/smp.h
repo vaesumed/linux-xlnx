4481caca3e76a17118c3b96f283a76d9f33e5207
Rusty Russell <rusty@rustcorp.com.au>
2009-06-15 09:39:12 +1000

    cpumask:remove-arch_send_call_function_ipi
    Now everyone is converted to arch_send_call_function_ipi_mask, remove
    the shim and the #defines.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/arm/include/asm/smp.h
+++ arch/arm/include/asm/smp.h
@@ -98,7 +98,6 @@ extern void platform_cpu_enable(unsigned int cpu);
 
 extern void arch_send_call_function_single_ipi(int cpu);
 extern void arch_send_call_function_ipi_mask(const struct cpumask *mask);
-#define arch_send_call_function_ipi_mask arch_send_call_function_ipi_mask
 
 /*
  * Local timer interrupt handling function (can be IPI'ed).
