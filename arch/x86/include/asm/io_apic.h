8c9e82ce65d7ba64d29555a762ab76355f45bc19
Feng Tang <feng.tang@intel.com>
2009-06-22 13:07:15 +0800

    SFI: expose IO-APIC routines to SFI, not just ACPI
    change condition from
    	#ifdef CONFIG_ACPI
    to
    	#if defined(CONFIG_ACPI) || defined(CONFIG_SFI)
    
    for several io_apic related APIs which will be used by both
    ACPI and SFI.
    
    this change will ensure the success kernel build whe
    CONFIG_SFI=y and CONFIG_ACPI=n
    
    Signed-off-by: Feng Tang <feng.tang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- arch/x86/include/asm/io_apic.h
+++ arch/x86/include/asm/io_apic.h
@@ -150,11 +150,14 @@ extern int timer_through_8259;
 #define io_apic_assign_pci_irqs \
 	(mp_irq_entries && !skip_ioapic_setup && io_apic_irqs)
 
-#ifdef CONFIG_ACPI
+#if defined(CONFIG_ACPI) || defined(CONFIG_SFI)
 extern int io_apic_get_unique_id(int ioapic, int apic_id);
 extern int io_apic_get_version(int ioapic);
 extern int io_apic_get_redir_entries(int ioapic);
-#endif /* CONFIG_ACPI */
+#endif /* CONFIG_ACPI || CONFIG_SFI */
+
+extern int io_apic_set_pci_routing(int ioapic, int pin, int irq,
+				   int edge_level, int active_high_low);
 
 struct io_apic_irq_attr;
 extern int io_apic_set_pci_routing(struct device *dev, int irq,
