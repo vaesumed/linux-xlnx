f968612b9155ae8cd72b8e0ec9b2bc8d993acc03
Stephen Rothwell <sfr@canb.auug.org.au>
2009-06-24 16:26:21 +1000

    Revert "cpumask:use-mm_cpumask-arm"
    This reverts commit e97df8b735e20500a1d6a3743bef153869c472d3.

--- arch/arm/include/asm/mmu_context.h
+++ arch/arm/include/asm/mmu_context.h
@@ -101,15 +101,14 @@ switch_mm(struct mm_struct *prev, struct mm_struct *next,
 
 #ifdef CONFIG_SMP
 	/* check for possible thread migration */
-	if (!cpumask_empty(mm_cpumask(next)) &&
-	    !cpumask_test_cpu(cpu, mm_cpumask(next)))
+	if (!cpus_empty(next->cpu_vm_mask) && !cpu_isset(cpu, next->cpu_vm_mask))
 		__flush_icache_all();
 #endif
-	if (!cpumask_test_and_set_cpu(cpu, mm_cpumask(next)) || prev != next) {
+	if (!cpu_test_and_set(cpu, next->cpu_vm_mask) || prev != next) {
 		check_context(next);
 		cpu_switch_mm(next->pgd, next);
 		if (cache_is_vivt())
-			cpumask_clear_cpu(cpu, mm_cpumask(prev));
+			cpu_clear(cpu, prev->cpu_vm_mask);
 	}
 #endif
 }
